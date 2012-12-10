48dfe72514bb500316cd459e0b6173220ba7d154
Ninjoncrack <mirek@gmail.com>
2012-12-10 08:21:55 -0800

    p32 slight faster, keep coin array global

--- src/p31.py
+++ src/p31.py
@@ -2,20 +2,20 @@ from datetime import datetime
 import math
 
 # in each recursive call, only use coins smaller than the ones just tried
-def numWays(remains, coins):
-    if remains < 0 or (len(coins) == 0 and remains > 0):
+def numWays(remains, index):
+    global coins
+    if remains < 0 or (index == -1 and remains > 0):
         return 0
     elif remains == 0:
         return 1
     else:
-        highest = coins.pop()
+        highest = coins[index]
         total = 0
         for i in range(0, math.floor(remains/highest) + 1):
-            coins_copy = list(coins)
-            total += numWays(remains - i*highest, coins_copy)
+            total += numWays(remains - i*highest, index - 1)
         return total
 
 startTime = datetime.now()
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
-print(numWays(200, coins))
+print(numWays(200, len(coins) - 1))
 print(datetime.now() - startTime)
