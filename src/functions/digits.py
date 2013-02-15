ed801ec28b24932531b636a988445d3aeba57a34
Mirek Kukla <mirek@addepar.com>
2013-02-15 10:18:02 -0800

    Add efficinet method to get binary string from num. TODO: extend numdigits to take a base

--- src/functions/digits.py
+++ src/functions/digits.py
@@ -18,3 +18,12 @@ def getNumFromArray(arr):
 
 def numDigits(n):
     return int(log10(n)) + 1
+
+def getBinaryStrFromNum(n): 
+    numBinDigits = (int(log(n,2)) + 1)
+    ret = [0]*numBinDigits
+    for i in range(numBinDigits):
+        ret[i] = str(n % 2)
+        n = n / 2
+    ret.reverse()
+    return ''.join(ret)
